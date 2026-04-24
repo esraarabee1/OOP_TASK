121f3b9c9056dbe1605af66f6347ca152270db0c
esraarabee1 <esraarabee69@gmail.com>
2026-04-24 10:15:31 -0700

    update readme file

--- main.cpp
+++ main.cpp
@@ -4,16 +4,14 @@ int main()
 {
 
     Car car = CarFactory::createCar("mixedEngine");
-
     car.start();
-
     car.accelerate();
     car.accelerate();
     car.accelerate();
     car.accelerate();
     car.accelerate();
     car.accelerate();
-
+    car.replaceEngine(new GasolineEngine());
     car.stop();
 
     return 0;
